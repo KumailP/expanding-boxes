21edfa6eadd238158f1fa87d4cd886a34fbb514e
Kumail Pirzada <kumailpirzada@gmail.com>
2019-02-05 00:17:23 +0500

    Created a util function to generate 30 random rectangles

---
+++ app/utils/generateRectangles/generateRectangles.js
@@ -0,0 +1,25 @@
+// using a random color generator by Anatoliy from StackOverflow
+// https://stackoverflow.com/questions/1484506/random-color-generator
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+//
+
+export default function generate() {
+  const rectangles = [];
+
+  for (let i = 0; i < 30; i++) {
+    const rectangle = {
+      height: parseInt(Math.random() * 300, 10),
+      color: getRandomColor()
+    };
+    rectangles.push(rectangle);
+  }
+
+  return rectangles;
+}
