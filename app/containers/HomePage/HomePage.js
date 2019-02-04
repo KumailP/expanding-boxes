9dc7950baa9e48fafc9ad0e6d318e3546bb5889d
Kumail Pirzada <kumailpirzada@gmail.com>
2019-02-05 02:38:34 +0500

    Updated UI to display rectangles in masonry format

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -9,18 +9,64 @@ import PropTypes from "prop-types";
 import "./style.scss";
 
 export default class HomePage extends React.PureComponent {
-  // eslint-disable-line react/prefer-stateless-function
-  /**
-   * when initial state username is not null, submit the form to load repos
-   */
-  componentDidMount() {
-    this.props.onGenerateRectangles();
+  constructor(props) {
+    super(props);
+    this.state = {
+      rectangles: []
+    };
   }
 
-  render() {
-    console.log(this.props.rectangles.size != 0);
+  componentDidMount() {
+    this.props.onGenerateRectangles();
+    window.addEventListener("resize", this.updateComponent);
+  }
 
-    return <div>Homepage</div>;
+  componentDidUpdate() {
+    if (this.props.rectangles.size != 0) {
+      this.setState({ rectangles: this.props.rectangles });
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateComponent);
+  }
+
+  updateComponent = () => {
+    this.forceUpdate();
+  };
+
+  getColumnCount = () => {
+    let browserWidth = Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+
+    let maxCols = 1;
+    if (Math.floor(browserWidth / 220) > maxCols) {
+      maxCols = Math.floor(browserWidth / 220);
+    }
+    return maxCols;
+  };
+
+  render() {
+    console.log("Rectangle #:", this.props.match.params.id);
+
+    const { rectangles } = this.state;
+
+    return (
+      <div className="home" style={{ columnCount: this.getColumnCount() }}>
+        {rectangles.map((rect, i) => (
+          <div
+            key={i}
+            className="rect"
+            style={{ height: `${rect.height}px`, backgroundColor: rect.color }}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
