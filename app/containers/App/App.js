6f2f1eb9f84d7696fc14f57f7a3b92dae46ccdb2
Kumail Pirzada <kumailpirzada@gmail.com>
2019-02-04 13:41:29 +0500

    Removed unnecessary boilerplate code

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -6,32 +6,17 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-import './style.scss';
+import HomePage from "containers/HomePage/Loadable";
+import "./style.scss";
 
 const App = () => (
   <div className="app-wrapper">
-    <Helmet
-      titleTemplate="%s - React.js Boilerplate"
-      defaultTitle="React.js Boilerplate"
-    >
-      <meta name="description" content="A React.js Boilerplate application" />
-    </Helmet>
-    <Header />
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="/features" component={FeaturePage} />
-      <Route path="" component={NotFoundPage} />
     </Switch>
-    <Footer />
   </div>
 );
 
--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -4,13 +4,12 @@
  * This is the first thing users see of our App, at the '/' route
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import ReposList from 'components/ReposList';
-import './style.scss';
+import React from "react";
+import PropTypes from "prop-types";
+import "./style.scss";
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class HomePage extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
   /**
    * when initial state username is not null, submit the form to load repos
    */
@@ -22,48 +21,13 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
   }
 
   render() {
-    const {
-      loading, error, repos, username, onChangeUsername, onSubmitForm
-    } = this.props;
+    const { loading, error } = this.props;
     const reposListProps = {
       loading,
-      error,
-      repos
+      error
     };
 
-    return (
-      <article>
-        <Helmet>
-          <title>Home Page</title>
-          <meta name="description" content="A React.js Boilerplate application homepage" />
-        </Helmet>
-        <div className="home-page">
-          <section className="centered">
-            <h2>Start your next react project in seconds</h2>
-            <p>
-              A minimal <i>React-Redux</i> boilerplate with all the best practices
-            </p>
-          </section>
-          <section>
-            <h2>Try me!</h2>
-            <form onSubmit={onSubmitForm}>
-              <label htmlFor="username">
-                Show Github repositories by
-                <span className="at-prefix">@</span>
-                <input
-                  id="username"
-                  type="text"
-                  placeholder="flexdinesh"
-                  value={username}
-                  onChange={onChangeUsername}
-                />
-              </label>
-            </form>
-            <ReposList {...reposListProps} />
-          </section>
-        </div>
-      </article>
-    );
+    return <div>Homepage</div>;
   }
 }
 
